31fe2714fd147af3005bdc035383eb954729f525
cdcMully (LT) <41015853+cdcMully@users.noreply.github.com>
2019-07-25 12:16:26 -0400

    Functional API calls and partial working main menu list.

--- API Program/Assets/Scripts/API.cs
+++ API Program/Assets/Scripts/API.cs
@@ -6,15 +6,16 @@ using UnityEngine.UI;
 
 public class API : MonoBehaviour
 {
-    private const string URL = "https://pokeapi.co/api/v2/";
+    private const string URL = "https://pokeapi.co/api/v2/pokemon/";
     private const string IMG_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png";
     public Text responseText;
     public Image responseImage;
+    private RootObject jsonResponse;
+    public InputField mainInputField;
 
     public void Request()
-    {
-        StartCoroutine(getTextCo(URL));
-        StartCoroutine(getImageCo(IMG_URL));
+    {        
+        StartCoroutine(getTextCo(URL + mainInputField.text.ToString().ToLower()));        
     }
 
     private IEnumerator getTextCo(string URL)
@@ -22,19 +23,177 @@ public class API : MonoBehaviour
         WWW req = new WWW(URL);
 
         yield return req;
+        
+        jsonResponse = RootObject.CreateFromJSON(req.text);
 
-        responseText.text = req.text;
+        yield return jsonResponse;
+
+        responseText.text = jsonResponse.name.ToUpper();
+
+        StartCoroutine(getImageCo(jsonResponse.sprites.front_default));
+
+        //Debug.Log("Number of moves: " + jsonResponse.moves.Count);
+
+        //foreach (Move move in jsonResponse.moves)
+        //{
+        //    Debug.Log(move.move.name);
+        //}
     }
 
     private IEnumerator getImageCo(string URL)
     {
         WWW req = new WWW(URL);
 
-        yield return req;        
-
-        //responseText.text = req.text;
+        yield return req;                
 
         responseImage.sprite = Sprite.Create(req.texture, new Rect(0, 0, req.texture.width, req.texture.height), new Vector2(0, 0));
+    }
+}
 
+[System.Serializable]
+public class Ability2
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class Ability
+{
+    public Ability2 ability;
+    public bool is_hidden;
+    public int slot;
+}
+
+[System.Serializable]
+public class Form
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class Version
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class GameIndice
+{
+    public int game_index;
+    public Version version;
+}
+
+[System.Serializable]
+public class Move2
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class MoveLearnMethod
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class VersionGroup
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class VersionGroupDetail
+{
+    public int level_learned_at;
+    public MoveLearnMethod move_learn_method;
+    public VersionGroup version_group;
+}
+
+[System.Serializable]
+public class Move
+{
+    public Move2 move;
+    public List<VersionGroupDetail> version_group_details;
+}
+
+[System.Serializable]
+public class Species
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class Sprites
+{
+    public string back_default;
+    public object back_female;
+    public string back_shiny;
+    public object back_shiny_female;
+    public string front_default;
+    public object front_female;
+    public string front_shiny;
+    public object front_shiny_female;
+}
+
+[System.Serializable]
+public class Stat2
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class Stat
+{
+    public int base_stat;
+    public int effort;
+    public Stat2 stat;
+}
+
+[System.Serializable]
+public class Type2
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class Type
+{
+    public int slot;
+    public Type2 type;
+}
+
+[System.Serializable]
+public class RootObject
+{
+    public List<Ability> abilities;
+    public int base_experience;
+    public List<Form> forms;
+    public List<GameIndice> game_indices;
+    public int height;
+    public List<object> held_items;
+    public int id;
+    public bool is_default;
+    public string location_area_encounters;
+    public List<Move> moves;
+    public string name;
+    public int order;
+    public Species species;
+    public Sprites sprites;
+    public List<Stat> stats;
+    public List<Type> types;
+    public int weight;
+
+    public static RootObject CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<RootObject>(jsonString);
     }
 }
---
+++ API Program/Assets/Scripts/WWW_GetAllPokemonScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WWW_GetAllPokemonScript : MonoBehaviour
+{
+    private const string URL = "https://pokeapi.co/api/v2/pokemon/?limit=964";
+    private RootObject2 jsonResponse;
+
+    public List<Result> pokemonList;
+    public GameObject buttonListItem;
+    public GameObject contentWindow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetAllPokemonAPICallCo(URL));
+    }    
+
+    private IEnumerator GetAllPokemonAPICallCo (string URL)
+    {
+        WWW request = new WWW(URL);
+
+        yield return request;
+
+        jsonResponse = RootObject2.CreateFromJSON(request.text);
+
+        yield return jsonResponse;
+
+        foreach (Result result in jsonResponse.results)
+        {
+            pokemonList.Add(result);
+        }
+
+        foreach (Result pokemon in pokemonList)
+        {
+            GameObject listItem = Instantiate(buttonListItem, new Vector2(0, 0), Quaternion.identity);
+            listItem.transform.SetParent(contentWindow.transform);
+            listItem.name = pokemon.name;
+        }
+    }
+}
+
+[System.Serializable]
+public class Result
+{
+    public string name;
+    public string url;
+}
+
+[System.Serializable]
+public class RootObject2
+{
+    public int count;
+    public object next;
+    public object previous;
+    public List<Result> results;
+
+    public static RootObject2 CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<RootObject2>(jsonString);
+    }
+}
